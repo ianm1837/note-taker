9f3bc19312d94fc35f391731cd363c12a87f7955
ianm1837 <ian@iwm.dev>
2023-02-27 11:43:37 -0800

    fix delete and post routes

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.listen(PORT);
 console.info('Server on port', PORT);
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -29,24 +31,39 @@ app.get('/api/notes', (req, res) => {
   res.json(getNotes());
 });
 
-app.get('*', (req, res) => {
-  res.send('what???', 404);
-});
-
 app.post('/api/notes', (req, res) => {
   const notes = getNotes();
-  const newNote = {
-    title: req.body.title,
-    text: req.body.text,
-  };
+
+  let noteID = 0;
+
+  for (const note of notes) {
+    console.log(note);
+    noteID = note.id;
+  }
+
+  noteID++;
+
+  let newNote = Object.create(req.body);
+  newNote.id = noteID;
+  newNote.title = req.body.title;
+  newNote.text = req.body.text;
+
   notes.push(newNote);
+
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+  noteID = 0;
+
   res.status(200).json({ message: 'Note saved' });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   const notes = getNotes();
-  const newNotes = notes.filter((note) => note.id !== req.params.id);
+  const newNotes = notes.filter((note) => note.id != req.params.id);
   fs.writeFileSync('./db/db.json', JSON.stringify(newNotes));
   res.status(200).json({ message: 'Note deleted' });
+});
+
+app.get('*', (req, res) => {
+  res.send('what???', 404);
 });
